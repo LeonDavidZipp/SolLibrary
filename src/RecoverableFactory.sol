c19316707e90a470de6723af20e8cf7a0100143b
Leon David Zipp <leondavidzipp@gmx.de>
2024-09-26 10:33:04 +0200

    added initial withrawal multisig functionality

--- src/RecoverableFactory.sol
+++ src/RecoverableFactory.sol
@@ -14,6 +14,10 @@ contract RecoverableFactory is OwnableMultisigNonce {
     uint256 private _gasLimit;
     address[] private _recoveries;
     mapping(address => address) private _recoveryMap;
+    uint256 private _withdrawBitmap;
+    uint256 private _abortWithdrawBitmap;
+    uint256 private _withdrawAllBitmap;
+    uint256 private _abortWithdrawAllBitmap;
 
     /* ********************************************************************** */
     /* Events                                                                 */
@@ -24,7 +28,7 @@ contract RecoverableFactory is OwnableMultisigNonce {
     /* Errors                                                                 */
     /* ********************************************************************** */
     error InsufficientFee();
-    error InvalidAddress();
+    error InvalidAddress(address);
     error RecoveryExists(address owner, address recovery);
 
     /* ********************************************************************** */
@@ -41,40 +45,43 @@ contract RecoverableFactory is OwnableMultisigNonce {
         payable
         OwnableMultisigNonce(initialOwner, initialSigners)
     {
-        _fee = initialFee;
-        _gasLimit = initialGasLimit;
+        assembly {
+            sstore(_fee.slot, initialFee)
+            sstore(_gasLimit.slot, initialGasLimit)
+            sstore(_withdrawBitmap.slot, 1)
+            sstore(_abortWithdrawBitmap.slot, 1)
+            sstore(_withdrawAllBitmap.slot, 1)
+            sstore(_abortWithdrawAllBitmap.slot, 1)
+        }
     }
 
     /* ********************************************************************** */
     /* Recovery Functions                                                     */
     /* ********************************************************************** */
-    function createRecovery(
-        address initialOwner,
-        address initialBackup,
-        uint256 nonce
-    ) external payable {
+    function createRecovery(address initialBackup, uint256 nonce) external payable returns (address recoveryAddr) {
         if (msg.value < fee()) {
             revert InsufficientFee();
         }
-        if (initialOwner == address(0)) {
-            revert InvalidAddress();
-        }
+
         if (initialBackup == address(0)) {
             revert InvalidAddress();
         }
-        if (recoveryAddress(initialOwner) != address(0)) {
-            revert RecoveryExists(initialOwner, recoveryAddress(initialOwner));
+        address owner_ = _msgSender();
+        if (recoveryAddress(owner_) != address(0)) {
+            revert RecoveryExists(owner_, recoveryAddress(owner_));
         }
 
-        Recoverable recovery = new Recoverable(initialOwner, initialBackup);
+        Recoverable recovery = new Recoverable(owner_, initialBackup);
 
-        address newAddr = address(recovery);
-        _recoveries.push(newAddr);
-        _recoveryMap[initialOwner] = newAddr;
+        recoveryAddr = address(recovery);
+        _recoveries.push(recoveryAddr);
+        _recoveryMap[owner_] = recoveryAddr;
 
-        emit RecoveryCreated(initialOwner, initialBackup, newAddr);
+        emit RecoveryCreated(owner_, initialBackup, recoveryAddr);
 
-        _useCheckedNonce(_msgSender(), nonce);
+        _useCheckedNonce(owner_, nonce);
+
+        return recoveryAddr;
     }
 
     function recoveryAddresses() public view returns (address[] memory r) {
@@ -98,26 +105,78 @@ contract RecoverableFactory is OwnableMultisigNonce {
         }
     }
 
-    function setFee(uint256 newFee) external onlyOwner {
+    function setFee(uint256 newFee, uint256 nonce) external onlyOwner notBlocked {
         assembly {
             sstore(_fee.slot, newFee)
         }
+
+        _useCheckedNonce(owner(), nonce);
     }
 
     /* ********************************************************************** */
     /* Withdraw Functions                                                     */
     /* ********************************************************************** */
-    function withdrawBalance(address to) external onlyOwner {
-        payable(to).transfer(address(this).balance - gasLimit());
+    function proposeWithdraw(uint256 nonce) external onlyOwner {
+        address sender = _msgSender();
+
+        if (withdrawAllBitmap() != 1) {
+            revert SignerChangeInProgress();
+        }
+    }
+    
+    function proposeWithdrawAll(uint256 nonce) external onlyOwner {
+        payable(owner()).transfer(address(this).balance - gasLimit());
     }
 
+    function confirmWithdraw(uint256 nonce) external onlySigner {
+        address sender = _msgSender();
+        uint256 i = _signerIndex(sender);
+        if (i > 4) {
+            revert UnauthorizedAccount(sender);
+        }
+        if (changeSignerBitmap() != 1) {
+            revert SignerChangeInProgress();
+        }
+    }
+
+    function abortWithdraw(uint256 nonce) external onlyOwner {
+
+    }
+
+    function withdrawBitmap() public view returns (uint256 w) {
+        assembly {
+            w := sload(_withdrawalBitmap.slot)
+        }
+    }
+
+    function abortWithdrawBitmap() public view returns (uint256 a) {
+        assembly {
+            a := sload(_abortWithdrawalBitmap.slot)
+        }
+    }
+
+    function withdrawAllBitmap() public view returns (uint256 w) {
+        assembly {
+            w := sload(_withdrawalAllBitmap.slot)
+        }
+    }
+
+    function abortWithdrawAllBitmap() public view returns (uint256 a) {
+        assembly {
+            a := sload(_abortWithdrawalAllBitmap.slot)
+        }
+    }
+
+    /* ********************************************************************** */
+    /* Gas Limit Functions                                                    */
+    /* ********************************************************************** */
     function gasLimit() public view returns (uint256 g) {
         assembly {
             g := sload(_gasLimit.slot)
         }
     }
 
-    function setGasLimit(uint256 newGasLimit) external onlyOwner {
+    function setGasLimit(uint256 newGasLimit) external onlyOwner notBlocked {
         assembly {
             sstore(_gasLimit.slot, newGasLimit)
         }
