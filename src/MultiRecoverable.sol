392af878429b06df83e2947e4e7e8a7c1a8f7437
Leon David Zipp <leondavidzipp@gmx.de>
2024-09-29 11:13:43 +0200

    adjusting multirecoverable

--- src/MultiRecoverable.sol
+++ src/MultiRecoverable.sol
@@ -1,87 +1,184 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import {MultisigWalletNonce} from "./MultisigWalletNonce.sol";
-// import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
+import {MultisigWalletNonce} from "./MultisigWalletNonce.sol";
+import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
 
-// contract MultiRecoverable is MultisigWalletNonce {
-//     /* ********************************************************************** */
-//     /* State Variables                                                        */
-//     /* ********************************************************************** */
+contract MultiRecoverable is MultisigWalletNonce {
+    /* ********************************************************************** */
+    /* State Variables                                                        */
+    /* ********************************************************************** */
+    IAllowanceTransfer internal immutable _PERMIT_2 = IAllowanceTransfer(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+    uint256 private _fee; // 0.025 ether for now
+    address[] private _accounts; // registered accounts
+    mapping(address => address) private _accountToBackup; // account => backup
+    mapping(address => address) private _backupToAccount; // backup => account
 
-//     uint256 private _fee; // 0.025 ether for now
-//     mapping(address => address) private _recoveryMap;
+    /* ********************************************************************** */
+    /* Events                                                                 */
+    /* ********************************************************************** */
+    event FundsTransferred(address indexed account, address indexed backup);
+    event Registered(address indexed account, address indexed backup);
+    event BackupChanged(address indexed account, address indexed newBackup);
 
-//     /* ********************************************************************** */
-//     /* Events                                                                 */
-//     /* ********************************************************************** */
+    /* ********************************************************************** */
+    /* Errors                                                                 */
+    /* ********************************************************************** */
+    error InsufficientFee();
+    error AlreadyRegistered(address account);
+    error NotRegistered(address account);
 
-//     /* ********************************************************************** */
-//     /* Errors                                                                 */
-//     /* ********************************************************************** */
+    /* ********************************************************************** */
+    /* Constructor                                                            */
+    /* ********************************************************************** */
+    constructor(address initialOwner, address[4] memory initialSigners, uint256 fee_)
+        payable
+        MultisigWalletNonce(initialOwner, initialSigners)
+    {
+        assembly {
+            sstore(_fee.slot, fee_)
+        }
+    }
 
-//     /* ********************************************************************** */
-//     /* Fallback Functions                                                     */
-//     /* ********************************************************************** */
-//     receive() external payable {}
+    /* ********************************************************************** */
+    /* Account Functions                                                      */
+    /* ********************************************************************** */
+    function register(address initialBackup, IAllowanceTransfer.PermitBatch memory permitBatch, bytes memory signature)
+        external
+        payable
+    {
+        if (msg.value < fee()) {
+            revert InsufficientFee();
+        }
+        if (initialBackup == address(0)) {
+            revert InvalidAddress(initialBackup);
+        }
+        address sender = _msgSender();
+        if (backup(sender) != address(0)) {
+            revert AlreadyRegistered(sender);
+        }
 
-//     fallback() external payable {}
+        permit2().permit(sender, permitBatch, signature);
 
-//     /* ********************************************************************** */
-//     /* Constructor                                                            */
-//     /* ********************************************************************** */
-//     constructor(address initialOwner, address[4] memory initialSigners, uint256 fee_)
-//         payable
-//         MultisigWalletNonce(initialOwner, initialSigners)
-//     {
-//         assembly {
-//             sstore(_fee.slot, fee_)
-//         }
-//     }
+        _accounts.push(sender);
+        _accountToBackup[sender] = initialBackup;
+        _backupToAccount[initialBackup] = sender;
 
-//     /* ********************************************************************** */
-//     /* Account Functions                                                      */
-//     /* ********************************************************************** */
-//     function register(address initialBackup, IAllowanceTransfer.PermitBatch memory permitBatch, bytes memory signature)
-//         external payable
-//     {
-//         if (msg.value < fee()) {
-//             revert InsufficientFee();
-//         }
-//         if (initialBackup == address(0)) {
-//             revert InvalidAddress(initialBackup);
-//         }
+        emit Registered(sender, initialBackup);
+    }
 
-//         sender = _msgSender();
-//         // if
-//     }
+    function unregister(IAllowanceTransfer.PermitBatch memory permitBatch, bytes memory signature) external {
+        address sender = _msgSender();
+        if (backup(sender) == address(0)) {
+            revert NotRegistered(sender);
+        }
 
-//     /* ********************************************************************** */
-//     /* Permit Functions                                                       */
-//     /* ********************************************************************** */
+        permit2().permit(sender, permitBatch, signature);
 
-//     /* ********************************************************************** */
-//     /* Recovery Functions                                                     */
-//     /* ********************************************************************** */
-//     function transferToBackup(IAllowanceTransfer.AllowanceTransferDetails[] calldata transferDetails)
-//         external
-//         // onlyBackup
-//     {
-//         _PERMIT_2.transferFrom(transferDetails);
+        unchecked {
+            address[] storage accounts_ = accounts();
+            uint256 len = accounts_.length;
+            for (uint256 i; i < len; ++i) {
+                if (accounts_[i] == sender) {
+                    accounts_[i] = accounts_[len - 1];
+                    accounts_.pop();
+                    break;
+                }
+            }
+        }
+        delete _accountToBackup[sender];
+        delete _backupToAccount[backup(sender)];
 
-//         emit FundsTransferred(_msgSender(), backup());
-//     }
+        emit Registered(sender, initialBackup);
+    }
 
-//     /* ********************************************************************** */
-//     /* Fee Functions                                                          */
-//     /* ********************************************************************** */
-//     function fee() public view returns (uint256) {
-//         return _fee;
-//     }
+    function accounts() public view returns (address[] memory) {
+        return _accounts;
+    }
 
-//     function setFee(uint256 fee_) external onlyOwner {
-//         assembly {
-//             sstore(_fee.slot, fee_)
-//         }
-//     }
-// }
+    function account(address backup) public view returns (address a) {
+        a = _backupToAccount[backup];
+    }
+
+    /* ********************************************************************** */
+    /* Permit Functions                                                       */
+    /* ********************************************************************** */
+    function updatePermits(IAllowanceTransfer.PermitBatch memory permitBatch, bytes memory signature) external {
+        address sender = _msgSender();
+        if (backup(sender) == address(0)) {
+            revert NotRegistered(sender);
+        }
+        permit2().permit(sender, permitBatch, signature);
+    }
+
+    function permit2() internal view returns (IAllowanceTransfer i) {
+        i = _PERMIT_2;
+    }
+
+    /* ********************************************************************** */
+    /* Backup Functions                                                      */
+    /* ********************************************************************** */
+    function backup() public view returns (address a) {
+        a = _accountToBackup[_msgSender()];
+    }
+
+    function backup(address account) public view returns (address a) {
+        a = _accountToBackup[account];
+    }
+
+    function changeBackup(address newBackup, uint256 nonce) external {
+        address sender = _msgSender();
+        if (backup(sender) == address(0)) {
+            revert NotRegistered(sender);
+        }
+        if (newBackup == address(0)) {
+            revert InvalidAddress(newBackup);
+        }
+
+        _accountToBackup[sender] = newBackup;
+
+        emit BackupChanged(sender, newBackup);
+
+        _useCheckedNonce(sender, nonce);
+    }
+
+    /* ********************************************************************** */
+    /* Recovery Functions                                                     */
+    /* ********************************************************************** */
+    function recover(IAllowanceTransfer.AllowanceTransferDetails[] calldata transferDetails) external {
+        address sender = _msgSender();
+        address account_ = account(sender);
+        if (account_ == address(0)) {
+            revert NotRegistered(sender);
+        }
+
+        unchecked {
+            uint256 len = transferDetails.length;
+            for (uint256 i; i = 0; ++i) {
+                if (transferDetails.from != account_) {
+                    revert InvalidAccount(transferDetails.from);
+                }
+                if (transferDetails.to != sender) {
+                    revert InvalidAddress(transferDetails.to);
+                }
+            }
+        }
+
+        permit2().transferFrom(transferDetails);
+
+        emit FundsTransferred(account_, sender);
+    }
+
+    /* ********************************************************************** */
+    /* Fee Functions                                                          */
+    /* ********************************************************************** */
+    function fee() public view returns (uint256) {
+        return _fee;
+    }
+
+    function setFee(uint256 fee_) external onlyOwner {
+        assembly {
+            sstore(_fee.slot, fee_)
+        }
+    }
+}
