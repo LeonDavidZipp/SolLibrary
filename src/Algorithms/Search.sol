055f635d3f9fe7283caccf1ca21fb7e652f53b58
Leon David Zipp <leondavidzipp@gmx.de>
2024-11-10 10:49:48 +0100

    added Search library

---
+++ src/Algorithms/Search.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+library Search {
+    function bidirectionalSearch(uint256[] memory arr, uint256 key) internal pure returns (int256) {
+        unchecked {
+            uint256 len = arr.length;
+            uint256 h = len / 2;
+            uint256 revi;
+
+            for (uint256 i = 0; i <= h; ++i) {
+                if (arr[i] == key) {
+                    return int256(i);
+                }
+                revi = len - i - 1;
+                if (arr[revi] == key) {
+                    return int256(revi);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
