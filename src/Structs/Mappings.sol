5bee4f6526168a5335df0b9181a3e772415067b7
Leon David Zipp <leondavidzipp@gmx.de>
2024-11-10 11:00:05 +0100

    fixed remappings & imports

--- src/Structs/Mappings.sol
+++ src/Structs/Mappings.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "../Algorithms/Search.sol";
+import "algorithms/Search.sol";
 
 library Mappings {
     using Search for uint256[];
