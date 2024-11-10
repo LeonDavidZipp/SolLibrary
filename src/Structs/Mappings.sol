c1afe07f5377e21b7dbe6855b048eaa504040517
Leon David Zipp <leondavidzipp@gmx.de>
2024-11-10 10:49:20 +0100

    added unset

--- src/Structs/Mappings.sol
+++ src/Structs/Mappings.sol
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "../Algorithms/Search.sol";
+
 library Mappings {
+    using Search for uint256[];
+
     /* -------------------------------------------------------------------------- */
     /* Uint to Uint Maps                                                          */
     /* -------------------------------------------------------------------------- */
     struct UintUintMap {
-        mapping(uint256 => uint256) _values;
+        mapping(uint256 key => uint256 val) _items;
         uint256[] _keys;
-        mapping(uint256 => bool) _keyExists;
+        mapping(uint256 key => bool) _keyExists;
     }
 
     function set(UintUintMap storage map, uint256 key, uint256 value) internal {
-        map._values[key] = value;
+        map._items[key] = value;
         if (!map._keyExists[key]) {
             map._keyExists[key] = true;
             map._keys.push(key);
@@ -20,6 +24,15 @@ library Mappings {
     }
 
     function get(UintUintMap storage map, uint256 key) internal view returns (uint256) {
-        return map._values[key];
+        return map._items[key];
+    }
+
+    function unset(UintUintMap storage map, uint256 key) internal {
+        if (map._keyExists[key]) {
+            delete map._items[key];
+            delete map._keyExists[key];
+            map._keys[uint256(map._keys.bidirectionalSearch(key))] = map._keys[map._keys.length - 1];
+            map._keys.pop();
+        }
     }
 }
