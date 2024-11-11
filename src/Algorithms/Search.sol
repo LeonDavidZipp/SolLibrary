e67c56580fc3326a29abc23ff5d439a1545bfcbe
Leon David Zipp <leondavidzipp@gmx.de>
2024-11-11 10:43:37 +0100

    added bidirectional search

--- src/Algorithms/Search.sol
+++ src/Algorithms/Search.sol
@@ -21,4 +21,22 @@ library Search {
             return -1;
         }
     }
+
+    function bidirectionalSearch(address[] memory arr, address key) internal pure returns (int256) {
+        uint256 len = arr.length;
+        uint256 h = len / 2;
+        uint256 revi;
+
+        for (uint256 i = 0; i <= h; ++i) {
+            if (arr[i] == key) {
+                return int256(i);
+            }
+            revi = len - i - 1;
+            if (arr[revi] == key) {
+                return int256(revi);
+            }
+        }
+
+        return -1;
+    }
 }
