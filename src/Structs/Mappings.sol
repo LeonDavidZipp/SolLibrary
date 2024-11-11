1a26b2d3bd09aeb9211db98cfcf0e7bb70eef4c0
Leon David Zipp <leondavidzipp@gmx.de>
2024-11-11 10:43:00 +0100

    updated mappings

--- src/Structs/Mappings.sol
+++ src/Structs/Mappings.sol
@@ -5,6 +5,7 @@ import "algorithms/Search.sol";
 
 library Mappings {
     using Search for uint256[];
+    using Search for address[];
 
     /* -------------------------------------------------------------------------- */
     /* Uint to Uint Maps                                                          */
@@ -28,6 +29,36 @@ library Mappings {
     }
 
     function unset(UintUintMap storage map, uint256 key) internal {
+        if (map._keyExists[key]) {
+            delete map._items[key];
+            delete map._keyExists[key];
+            map._keys[uint256(map._keys.bidirectionalSearch(key))] = map._keys[map._keys.length - 1];
+            map._keys.pop();
+        }
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /* Address to Bytes Maps                                                      */
+    /* -------------------------------------------------------------------------- */
+    struct AddressBytesMap {
+        mapping(address key => bytes val) _items;
+        address[] _keys;
+        mapping(address key => bool) _keyExists;
+    }
+
+    function set(AddressBytesMap storage map, address key, bytes memory value) internal {
+        map._items[key] = value;
+        if (!map._keyExists[key]) {
+            map._keyExists[key] = true;
+            map._keys.push(key);
+        }
+    }
+
+    function get(AddressBytesMap storage map, address key) internal view returns (bytes memory) {
+        return map._items[key];
+    }
+
+    function unset(AddressBytesMap storage map, address key) internal {
         if (map._keyExists[key]) {
             delete map._items[key];
             delete map._keyExists[key];
